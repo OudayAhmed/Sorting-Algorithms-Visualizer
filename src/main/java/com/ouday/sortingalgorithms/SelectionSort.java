ec181248ddf30e2cb5bfdda711fb085d5ca8292d
OudayAhmed <oydddua@gmail.com>
2020-11-22 22:08:36 +0100

    SelectionSort added

---
+++ src/main/java/com/ouday/sortingalgorithms/SelectionSort.java
@@ -0,0 +1,49 @@
+package com.ouday.sortingalgorithms;
+
+public class SelectionSort {
+
+    private int[] array;
+    private int sortingIndex = 0;
+    private int searchMinimumIndex = 1;
+    private int minimumIndex = sortingIndex;
+    private int size;
+
+    public SelectionSort(int[] array) {
+        this.array = array;
+        size = this.array.length;
+    }
+
+    public void oneStepSortingSelectionSort() {
+        if (sortingIndex < size - 1) {
+            if (searchMinimumIndex < size) {
+                if (array[minimumIndex] > array[searchMinimumIndex]) {
+                    minimumIndex = searchMinimumIndex;
+                }
+                searchMinimumIndex++;
+            } else if (searchMinimumIndex == size){
+                swap();
+                sortingIndex++;
+                searchMinimumIndex = sortingIndex + 1;
+                minimumIndex = sortingIndex;
+            }
+        }
+    }
+
+    private void swap() {
+        int temp = array[sortingIndex];
+        array[sortingIndex] = array[minimumIndex];
+        array[minimumIndex] = temp;
+    }
+
+    public int getSortingIndex() {
+        return sortingIndex;
+    }
+
+    public int getSearchMinimumIndex() {
+        return searchMinimumIndex;
+    }
+
+    public int getMinimumIndex() {
+        return minimumIndex;
+    }
+}
